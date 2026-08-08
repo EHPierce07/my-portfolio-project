20613b5ca01907995edbb2d58fd948233309501a
EHPierce07 <pierceely17@gmail.com>
2026-08-08 13:51:32 -0400

    Delete SchoolSubjects

---
+++ MultidimensionalArrays.java
@@ -0,0 +1,10 @@
+public class MusicPlaylist {
+    public static void main(String[] args){
+
+        String genre[] = {"Pop", "Rock", "Hip-hop", "Kpop", "Classical"};
+
+        String music[][] = {
+            {"Pop", "Golden", "Lollipop" },//pop
+            {"Crybaby", "Himala", "Hinahanap-hanap Kita" }
+        }    }
+}
